9369a86533051cd17393f25855e4e9ce422b30a3
Surya <work.suryacn@gmail.com>
2026-07-23 07:58:41 +0530

    chore: remove temporary debug scripts

--- find_post.ts
+++
@@ -1,8 +0,0 @@
-import { prisma } from "./src/lib/prisma.ts";
-
-async function main() {
-  const posts = await prisma.$queryRaw`SELECT * FROM raw_posts WHERE content LIKE '%why crashed game%'`;
-  console.log(JSON.stringify(posts, null, 2));
-}
-
-main().catch(console.error).finally(() => prisma.$disconnect());
